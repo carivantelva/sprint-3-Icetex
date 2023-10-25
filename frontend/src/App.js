8f1004b68111c2fb4f9b9ba8a5fa3af17688210d
carivantelva <carlostello_25@hotmail.com>
2023-10-25 06:56:39 -0500

    se agrega nav en index ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
 
 export const App = () => {
-  return(
-    <div className="App">
-      <header className="App-header">
-        <h1>desde App react (index)</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Esto se puede edtar <code>Hola</code> como.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          App Tello vargas
-        </a>
-      </header>
-    </div>
-  );
-}
+  return (
+    <Fragment>
+      <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+        <div className="container d-flex justify-content-between align-items-center">
+          <div>
+            <a className="nav-link" href="/">Ropero Lily</a>
+            
+          </div>
+          <div>
+          <a className="nav-link" href="/">Inicio  </a>
+            
+          </div>
+          <div><a className="nav-link" href="/prendas">Prendas</a></div>
+          <form className="d-flex">
+          
+            <input className="form-control me-2" type="search" placeholder="Búsqueda" aria-label="Búsqueda" />
+            <button className="btn btn-outline-success" type="submit">Buscar</button>
+          </form>
+          <div>
+            <a className="nav-link" href="/login">Inicio Sesión</a>
+          </div>
+        </div>
+      </nav>
+      <section id="informacion-sitio" className="bg-light p-4">
+      <div className="container">
+        <div className="container mt-4">
+          <div className="row" id="productos-container">
+            {/* Los productos se cargarán aquí dinámicamente */}
 
-export default App;
+            <h1>aqui van los productos cargado dinamicamente</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    </Fragment>
+  );
+};
